d8cb5491c607fadc2dd1a31a3a3bed604b116a50
treere <tomasiandrea.at@gmail.com>
2023-08-02 16:00:00 +0200

    Color example link

--- example.exs
+++ example.exs
@@ -43,7 +43,12 @@ sheet1 =
   |> Sheet.set_cell("E6", {:formula, "SUM(E1:E5)", value: 15.70}, num_format: "0.00", bold: true)
   |> Sheet.set_cell("F1", {:formula, "NOW()"}, num_format: "yyyy-mm-dd hh:MM:ss")
   |> Sheet.set_col_width("F", 18.0)
-  |> Sheet.set_cell("F2", {:link, {"http://www.google.it", "google.it"}})
+  |> Sheet.set_cell(
+    "F2",
+    {:link, {"http://www.google.it", "google.it"}},
+    underline: true,
+    color: "#0000ff"
+  )
   # Data validations
   |> Sheet.set_cell("A1", "dog")
   |> Sheet.set_cell("A2", "cat")
